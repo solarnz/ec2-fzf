00e75a2065b008119441d5d078c75b159e4fbc32
Chris Trotman <chris@trotman.io>
2016-12-24 18:31:21 +1100

    Initial commit of ec2-fzf

---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/solarnz/fzf/src"
+)
+
+type Ec2fzf struct {
+	ec2      *ec2.EC2
+	fzfInput *bytes.Buffer
+}
+
+var region string
+var usePrivateIp bool
+
+func main() {
+	flag.StringVar(&region, "region", "us-east-1", "The AWS region")
+	flag.BoolVar(&usePrivateIp, "private", false, "return the private IP address of the instance rather than the public dns")
+	flag.Parse()
+
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String(region),
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ec2fzf := Ec2fzf{
+		ec2:      ec2.New(sess),
+		fzfInput: new(bytes.Buffer),
+	}
+
+	instances, err := ec2fzf.listInstances()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, i := range instances {
+		ec2fzf.fzfInput.WriteString(StringFromInstance(i))
+		ec2fzf.fzfInput.WriteString("\n")
+	}
+
+	options := fzf.ParseOptions()
+	options.Header = []string{
+		"AWS EC2 Instances",
+	}
+	options.Multi = false
+	options.Input = ec2fzf.fzfInput
+	options.Printer = func(str string) {
+		i, err := InstanceIdFromString(str)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		address, err := ec2fzf.GetConnectionDetails(i)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf(address)
+	}
+	fzf.Run(options)
+}
+
+func (e *Ec2fzf) listInstances() ([]*ec2.Instance, error) {
+	instances := make([]*ec2.Instance, 0, 0)
+	params := &ec2.DescribeInstancesInput{}
+
+	err := e.ec2.DescribeInstancesPages(
+		params,
+		func(p *ec2.DescribeInstancesOutput, lastPage bool) bool {
+			for _, r := range p.Reservations {
+				for _, i := range r.Instances {
+					instances = append(instances, i)
+				}
+			}
+			return !lastPage
+		},
+	)
+
+	return instances, err
+}
+
+func (e *Ec2fzf) GetConnectionDetails(instanceId string) (string, error) {
+	params := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String(instanceId)},
+	}
+	resp, err := e.ec2.DescribeInstances(params)
+	if err != nil {
+		return "", err
+	}
+
+	if !(len(resp.Reservations) == 1) || !(len(resp.Reservations[0].Instances) == 1) {
+		return "", fmt.Errorf("No instance could be found for %s", instanceId)
+	}
+
+	if usePrivateIp {
+		return *resp.Reservations[0].Instances[0].PrivateIpAddress, nil
+	}
+	return *resp.Reservations[0].Instances[0].PublicDnsName, nil
+}
+
+func StringFromInstance(i *ec2.Instance) string {
+	tags := make([]string, 0, 0)
+	for _, t := range i.Tags {
+		tags = append(tags, fmt.Sprintf("%s=%s", *t.Key, *t.Value))
+	}
+	return fmt.Sprintf("%s: Tags=(%s)", *i.InstanceId, strings.Join(tags, " "))
+}
+
+func InstanceIdFromString(s string) (string, error) {
+	i := strings.Index(s, ":")
+	if i < 0 {
+		return "", fmt.Errorf("Unable to find instance id")
+	}
+	return s[0:i], nil
+}
